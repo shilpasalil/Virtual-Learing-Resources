35dd37c9d9b60c1220fbb2d3e81394915c255bdd
shilpa rao <shilpasalil@gmail.com>
2020-08-23 19:25:14 -0500

    initial commit

---
+++ src/main/java/org/launchcode/VirtualLearningResources/VirtualLearningResourcesApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.VirtualLearningResources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualLearningResourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualLearningResourcesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/VirtualLearningResources/VirtualLearningResourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.VirtualLearningResources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualLearningResourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
